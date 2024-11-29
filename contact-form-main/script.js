0950343201f11785668f270eb192563584cbcee0
Pascal-K <karsten.pas@gmail.com>
2024-11-29 22:13:39 +0100

    radio event handler

---
+++ contact-form-main/script.js
@@ -0,0 +1,82 @@
+const form = document.getElementById('form');
+
+const cGeneral = document.getElementById('cGeneral');
+const cSupport = document.getElementById('cSupport');
+const generalRadio = document.getElementById('general');
+const supportRadio = document.getElementById('support');
+const btnSubmit = document.getElementById('btn-submit');
+const cSuccess =  document.getElementById('cSuccess');
+
+//error messages
+const errorFirstName = document.getElementsByID('errorFirstName');
+const errorLastName = document.getElementsByID('rrorLastName');
+const errorEmail = document.getElementsByID('errorEmail');
+const errorQuery = document.getElementsByID('errorQuery');
+const errorMessage = document.getElementsByID('errorMessage');
+const errorCheck = document.getElementsByID('errorCheck');
+
+
+//get custom colors from stylesheet
+const rootColors = getComputedStyle(document.documentElement);
+const green = rootColors.getPropertyValue('--Green').trim();
+const greenDark = rootColors.getPropertyValue('--GreenDark').trim();
+
+//radios/radio(div) event handling
+generalRadio.addEventListener('change', () => {
+    if (generalRadio.checked == true) {
+        cGeneral.style.backgroundColor = green;
+        cGeneral.style.borderColor = greenDark;
+        cSupport.style.backgroundColor = '';
+        cSupport.style.borderColor = '';
+    }
+});
+supportRadio.addEventListener('change', () => {
+    if (supportRadio.checked == true) {
+        cSupport.style.backgroundColor = green;
+        cSupport.style.borderColor = greenDark;
+        cGeneral.style.backgroundColor = '';
+        cGeneral.style.borderColor = '';
+    }
+});
+cGeneral.addEventListener('click', () => {
+    generalRadio.checked = true;
+    generalRadio.dispatchEvent(new Event('change'));
+})
+cSupport.addEventListener('click', () => {
+    supportRadio.checked = true;
+    supportRadio.dispatchEvent(new Event('change'));
+})
+
+
+
+
+//Form success & error state handling
+
+//Show error messages under input fields
+const showErrorMessages = (input, message) => {
+    const element = document.getElementById(`${input.id}-error`);
+    if (element) {
+        element.removeAttribute('hidden');
+        element.innerText = message;
+        input.setAttribute('true');
+    }
+};
+
+
+
+btnSubmit.onclick = () => {
+    let errorCounter = 0;
+    for(let field of form.elements) {
+        let valid = field.validity.valid;
+        if (valid) {
+            continue;
+        }
+        showErrorMessages(field);
+        errorCounter++;
+    }
+    if (!errorCounter) {
+        clearErrorMessages()
+    }
+
+    //
+}
