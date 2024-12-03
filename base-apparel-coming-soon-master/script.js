684cb358677e5876f2446dd6496a89884111bef0
Pascal-K <karsten.pas@gmail.com>
2024-12-03 18:28:24 +0100

    error states

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,1 +1,26 @@
-const form = document.form();
+const form = document.form;
+
+form.onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Senden...');
+    console.log(e);
+
+    return validate();
+}
+
+
+function validate() {
+    if (form.email.value = '') {
+        console.log('Email fehlt...');
+        form.email.focus();
+        return false;
+    }
+    return true;
+}
+
+
+/* 
+<button>
+          <img src="images/icon-arrow.svg" alt="">
+        </button>
+*/
