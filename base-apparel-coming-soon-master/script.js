48e838ae975c5fe8c98f46490ac8d15b1871c014
Pascal-K <karsten.pas@gmail.com>
2024-12-04 21:18:06 +0100

    error handling fix

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,26 +1,23 @@
-const form = document.form;
+const form = document.querySelector('form[name="form"]');
+const errorMessage = document.getElementById('errorMessage');
+const inputContainer = document.getElementById('inputDiv');
 
-form.onSubmit = (e) => {
-    e.preventDefault();
-    console.log('Senden...');
+form.addEventListener('submit', (e) => {
+    console.log('Senden...')
     console.log(e);
+    return validate(e);
+});
 
-    return validate();
-}
 
-
-function validate() {
-    if (form.email.value = '') {
+function validate(e) {
+    if (form.email.value == '') {
         console.log('Email fehlt...');
+        form.errorLogo.style.display = 'block';  
+        errorMessage.style.display = 'block';
+        inputContainer.style.border = '1px solid red';
         form.email.focus();
+        e.preventDefault();
         return false;
     }
     return true;
 }
-
-
-/* 
-<button>
-          <img src="images/icon-arrow.svg" alt="">
-        </button>
-*/
